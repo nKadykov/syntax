b1465bdcb968fd314ea584db20a6b589bb2d2148
nKadykov <nkadykov@gmail.com>
2024-05-30 20:59:04 +0300

    fix: array_method

--- array_methods.js
+++ array_methods.js
@@ -1,3 +1,22 @@
 let arr = [1, 2, 3]
 arr.push(4, 5);
-console.log(arr)
+console.log(arr);
+
+let last = arr.pop();
+console.log(last);
+console.log(arr);
+
+let shift = arr.shift();
+console.log(first);
+console.log(arr);
+
+arr.unshift(0);
+console.log(arr);
+
+arr.splice(2, 1, 'a', 'b');
+console.log(arr);
+
+let arr1 = [1, 2]
+let arr2 = [3, 4]
+let combined = arr1.concat(arr2);
+console.log(combined);
