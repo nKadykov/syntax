301f1ed698fe9344987706f9a1263d4667608ec0
nKadykov <nkadykov@gmail.com>
2024-05-30 21:25:21 +0300

    fix: array_method

--- array_methods.js
+++ array_methods.js
@@ -31,3 +31,15 @@ arr.forEach(element => {
 let arr3 = [1, 2, 3];
 let mapped = arr3.map(x => x * 2);
 console.log(mapped);
+
+let arr4 = [1, 2, 3, 4, 5];
+let filtered = arr4.filter(x => x > 2);
+console.log(filtered);
+
+let arr5 = [1, 2, 3, 4];
+let sum = arr5.reduce((accumulator, currentValue) => accumulator, currentValue, 0);
+console.log(sum);
+
+let arr6 = [1, 2, 3, 4]
+let hasEven = arr6.some(x => x % 2 === 0);
+console.log(hasEven);
